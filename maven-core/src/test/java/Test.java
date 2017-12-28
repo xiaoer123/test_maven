8b6194c34a45da7159ef5738f05d1f31ecff835f
xiaoer123 <1194174677@qq.com>
2017-12-28 11:03:24 +0800

    Update Test.java

--- maven-core/src/test/java/Test.java
+++ maven-core/src/test/java/Test.java
@@ -17,6 +17,8 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("123");
+        System.out.println("那是当然的");
+        
 
         final InputStreamReader isr = new InputStreamReader(in);
         BufferedReader bufferedReader = new BufferedReader(isr);
