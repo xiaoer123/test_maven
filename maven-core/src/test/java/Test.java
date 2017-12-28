0837107be42c2213a138d60f2f96f0872b3280fc
wuyixiang <1194174677@qq.com>
2017-12-28 10:50:07 +0800

    测试

---
+++ maven-core/src/test/java/Test.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import static java.lang.System.in;
+
+public class Test {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("123");
+
+        final InputStreamReader isr = new InputStreamReader(in);
+        BufferedReader bufferedReader = new BufferedReader(isr);
+    }
+
+
+   /* public static void main(String[] args) {
+        final InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(in);
+
+    }*/
+
+    void greet(String message) {
+
+        try {
+            if (message != null) {
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
