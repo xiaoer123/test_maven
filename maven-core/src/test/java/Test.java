7c8d125daa4a2ee0e06c6e0032694421a91ed197
wuyixiang <1194174677@qq.com>
2017-12-28 11:05:47 +0800

    提交一下

--- maven-core/src/test/java/Test.java
+++ maven-core/src/test/java/Test.java
@@ -17,6 +17,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("123");
+        System.out.println("小吴真帅！");
 
         final InputStreamReader isr = new InputStreamReader(in);
         BufferedReader bufferedReader = new BufferedReader(isr);
